c3f240f0f43da0093d7cd45867b68c0ba2f4ea00
unknown <olwethumalumla31@gmail.com>
2024-11-08 15:10:09 +0200

    added home fotos

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -2,11 +2,11 @@
     <div id="content">
         <h2>WE SET THE STANDARDS OTHERS TRY TO LIVE UP TO</h2>
         <p>Providing excellent transport services in the Western Cape.</p>
-        <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
-        <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
-        <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
-        <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
-        <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
+        <img src="https://mquanaazi.github.io/huluImages/ezainbusiness.jpeg" alt="logo">
+        <img src="https://mquanaazi.github.io/huluImages/ezainbusiness1.jpeg" alt="logo">
+        <img src="https://mquanaazi.github.io/huluImages/ezaonmotion.jpeg" alt="logo">
+        <img src="https://mquanaazi.github.io/huluImages/ezatwins.jpeg" alt="logo">
+        <img src="https://mquanaazi.github.io/huluImages/ezatwins1.jpeg" alt="logo">
         <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
         <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
         <img src="https://mquanaazi.github.io/huluImages/eza express.png" alt="logo">
